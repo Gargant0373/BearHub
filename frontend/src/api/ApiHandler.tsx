63a5eb59f8dc23ce07ab84c0f8ab2a669ca5ae7e
Gargant <gargant0373@gmail.com>
2023-09-10 20:57:51 +0200

    Wrote basic beer requests.

--- frontend/src/api/ApiHandler.tsx
+++ frontend/src/api/ApiHandler.tsx
@@ -3,6 +3,38 @@ import axios from 'axios';
 const server_path = "localhost:4999"
 const api_path = "/api/v2"
 
-let increment = async (name: string, type: string) => {
+let getBeersData = async () => {
+    try {
+        const response = await axios.get(`${server_path}${api_path}/person`);
+        return response.data;
+    } catch (error: any) {
+        console.error(error.message);
+    }
+}
 
+let getBeerData = async (name: string) => {
+    try {
+        const response = await axios.get(`${server_path}${api_path}/person/${name}`);
+        return response.data;
+    } catch (error: any) {
+        console.error(error.message);
+    }
+}
+
+let incrementBeerData = async (name: string, type: string) => {
+    try {
+        await axios.post(`${server_path}${api_path}/person/${name}`, {
+            type: type
+        });
+    } catch (error: any) {
+        console.error(error.message);
+    }
+}
+
+let deleteBeerData = async (name: string) => {
+    try {
+        await axios.delete(`${server_path}${api_path}/person/${name}/delete`);
+    } catch (error: any) {
+        console.error(error.message);
+    }
 }
